831ec4028651ee385a1b2d5d5bdd17b9367eec7f
Constanza <csdelartundo@gmail.com>
2024-12-02 17:09:34 -0300

    Limpieza comentarios en MyProyects.vue

--- src/components/experience/MyProyects.vue
+++ src/components/experience/MyProyects.vue
@@ -8,12 +8,10 @@
       <!-- Primer proyecto -->
       <div
         class="flex flex-col items-center space-y-2 bg-blue-100 rounded-md p-3 shadow-sm hover:bg-blue-200 transition-colors">
-        <!-- Imagen del proyecto con tamaño reducido y efecto hover -->
         <div class="group relative">
           <img src="/src/assets/img/pokeapp.png" alt="Pokemon App"
             class="w-48 mb-6 h-auto transition-transform transform group-hover:scale-105" />
         </div>
-        <!-- Enlace del proyecto con estilo y efecto hover -->
         <a href="https://prueba-interfaz-interactiva-vue-js-pokemon.vercel.app/" target="_blank" class="text-blue-800 ">
           Pokemon App
         </a>
@@ -22,12 +20,10 @@
       <!-- Segundo proyecto -->
       <div
         class="flex flex-col items-center space-y-2 bg-blue-100 rounded-md p-3 shadow-sm hover:bg-blue-200 transition-colors">
-        <!-- Imagen del proyecto con tamaño reducido y efecto hover -->
         <div class="group relative">
           <img src="/src/assets/img/elchilenoweb.png" alt="Web El Chileno"
             class="w-48 h-auto transition-transform transform group-hover:scale-105" />
         </div>
-        <!-- Enlace del proyecto con estilo y efecto hover -->
         <a href="https://elchileno.vercel.app/" target="_blank"
           class="bg-blue-100 text-blue-800 rounded-md p-3 shadow-sm hover:bg-blue-200 transition-colors">
           Pyme "El Chileno Temático"
@@ -37,12 +33,10 @@
       <!-- Tercer proyecto -->
       <div
         class="flex flex-col items-center space-y-2 bg-blue-100 rounded-md p-3 shadow-sm hover:bg-blue-200 transition-colors">
-        <!-- Imagen del proyecto con tamaño reducido y efecto hover -->
         <div class="group relative">
           <img src="/src/assets/img/todolist.png" alt="To Do List"
             class="w-48 h-auto transition-transform transform group-hover:scale-105" />
         </div>
-        <!-- Enlace del proyecto con estilo y efecto hover -->
         <a href="https://elchileno.vercel.app/" target="_blank"
           class="bg-blue-100 text-blue-800 rounded-md p-3 shadow-sm hover:bg-blue-200 transition-colors">
           To Do List
